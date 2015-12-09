ae46f100e1d88d3aadc8ca0c26f26eca3ff27a2a
ekuryatenko <ekuryatenko@gmail.com>
2015-12-09 03:34:34 +0200

    Fractals added

---
+++ H_tree/H_tree.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include "console.h"
+#include <stdlib.h>
+#include "gbufferedimage.h"
+
+
+using namespace std;
+
+GBufferedImage* img;
+
+void buildH(GWindow& gw,
+                      int lengthCnt,
+                      int requiredOrder,
+                      GPoint& centerPt,
+                      double length,
+                      double angle){
+
+    if(requiredOrder >= 0){
+
+        lengthCnt++;
+        int koef = lengthCnt%2;
+
+        if(koef){ length = length / 2;
+        }else{requiredOrder--;}
+
+        GPoint pt1 = gw.drawPolarLine(centerPt, length, angle);
+        GPoint pt2 = gw.drawPolarLine(centerPt, length, (180 + angle));
+
+        /* Left brunch */
+        buildH(gw,
+                  lengthCnt,
+                  requiredOrder,
+                  pt1,
+                  (length),
+                  (angle + 90));
+        /* Right brunch */
+        buildH(gw,
+                  lengthCnt,
+                  requiredOrder,
+                  pt2,
+                  (length),
+                  (angle - 90));
+    }
+}
+
+
+int main() {
+    GWindow gw;
+    double length = 200;
+    int requiredOrder = 3;//It would be better range 0 - 6;
+    double gwWidth = 2 * length;
+    double gwHeight = 2 * length ;
+
+    gw.setSize(gwWidth, gwHeight);
+
+    int currentOrder = 0;
+    GPoint centerPt((gwWidth / 2), (gwHeight / 2));
+
+    buildH(gw,
+            currentOrder,
+            requiredOrder,
+            centerPt,
+            length,
+            0);
+
+   return 0;
+}
+
---
+++ Wallinger_tree/Wallinger_tree.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include "console.h"
+#include <stdlib.h>
+#include "gbufferedimage.h"
+#include "random.h"
+
+
+using namespace std;
+
+GBufferedImage* img;
+
+void buildTree(GWindow& gw,
+                      int currentOrder,
+                      int requiredOrder,
+                      GPoint& bottomPt,
+                      double length,
+                      double angle){
+
+    if(currentOrder <= requiredOrder){
+        currentOrder++;
+
+        GPoint topPt = gw.drawPolarLine(bottomPt, length, angle);
+
+        /* Left brunch */
+        buildTree(gw,
+                  currentOrder,
+                  requiredOrder,
+                  topPt,
+                  (length / (randomReal(1.2, 2))),
+                  (angle + 45 * (randomReal(0.7, 1.3))));
+        /* Right brunch */
+        buildTree(gw,
+                  currentOrder,
+                  requiredOrder,
+                  topPt,
+                  (length / (randomReal(1.2, 2))),
+                  (angle - 45 * (randomReal(0.7, 1.3))));
+    }
+}
+
+
+int main() {
+    GWindow gw;
+    double length = 200;
+    int requiredOrder = 10;//It would be better range 0 - 15;
+    double gwWidth = 3 * length;
+    double gwHeight = 3 * length ;
+
+    gw.setSize(gwWidth, gwHeight);
+
+    int currentOrder = 0;
+    GPoint bottomPt((gwWidth / 2), gwHeight);
+
+    buildTree(gw,
+                currentOrder,
+                requiredOrder,
+                bottomPt,
+                length,
+                90);
+    int width = gw.getWidth();
+    int height = gw.getHeight();
+    //gw.setSize(width, height);
+
+
+   return 0;
+}
+
